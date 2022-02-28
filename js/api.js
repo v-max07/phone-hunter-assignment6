4321f1deb676e1e465a595345ae24b39829cdf3c
v-max07 <vusandebnath09@gmail.com>
2022-03-01 05:10:49 +0600

    add show more botton successfully

--- js/api.js
+++ js/api.js
@@ -1,6 +1,7 @@
-function searchBtn(){
+function searchBtn(isSlice){
     const inputText = document.getElementById('inputBox');
     const searchText = inputText.value;
+    const sliceIs = isSlice;
     if (searchText == '') {
         const itemsDiv = document.getElementById('itemsDiv');
 
@@ -12,17 +13,24 @@ function searchBtn(){
     } else {
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
 
-    
         fetch(url)
             .then(res => res.json())
-            .then(data => displayItems(data.data))
+            .then(data => displayItems(data.data, sliceIs))
         
     }
     
 };
 
-const displayItems = (items) => {
+const displayItems = (items, isSlice) => {
     // console.log(items.length);
+    //chacking the upto 20 items 
+    if (items.length > 21 && isSlice) {
+        document.getElementById('showMoreBtn').style.display = 'block';
+    } else {
+        document.getElementById('showMoreBtn').style.display = 'none';
+    }
+
+    //chacking the input value has enough items for showing in UI.
     if (items.length === 0) {
         const itemsDiv = document.getElementById('itemsDiv');
 
@@ -34,13 +42,19 @@ const displayItems = (items) => {
     } else {
         const itemsDiv = document.getElementById('itemsDiv');
         itemsDiv.textContent = '';
-        const allItems = items.slice(0, 19)
+        
+        let allItems;
+        if (isSlice == true) {
+            allItems = items.slice(0, 20);
+        } else {
+            allItems = items;
+        }
         allItems.forEach(item => {
-        // console.log(item);
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('col-md-4');
+            // console.log(item);
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('col-md-4');
 
-        newDiv.innerHTML = `
+            newDiv.innerHTML = `
                 <div class="card text-center my-2 border-bottom border-danger p-4 divHeight">
                     <div class="phone-picture">
                         <img class="w-25" src="${item.image}" alt="">
@@ -52,6 +66,15 @@ const displayItems = (items) => {
             itemsDiv.appendChild(newDiv);
         });
     }
+ 
+};
 
-    
+
+
+
+
+// for see more items botton
+
+const getMoreItems = () => {
+    searchBtn(false);
 }
